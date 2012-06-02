98694c6f72b9deac91761bbf1910bcb0dd59e66c
Bharat Mhaskar <bmhaskar@gmail.com>
2012-06-02 15:38:25 +0530

    Corrected curl parameters

--- SprintPDF-api.php
+++ SprintPDF-api.php
@@ -4,9 +4,9 @@
 
     class sprintPDF {
        private $parameters = array();
-       private $gateway = 'http://ec2-50-19-62-60.compute-1.amazonaws.com/haapus/';
-       private $apiVersion = '?q=api';
-       private $resourceType = 'sprintpdf';
+       private $gateway = 'http://www.sprintpdf.com/';
+       private $apiVersion = 'api';
+       private $resourceType = 'pdf';
        private $url = "";
        private $outstream  = NULL;
        private $responseFunction = "defaultResponse";
@@ -21,51 +21,46 @@
           $this->url = $this->gateway.$this->apiVersion.'/'.$this->resourceType;
        }
 
-       function convertURI( $src, $outstream = null) {
+       function convertURI( $src, $outstream = null,$apiParameters = null ) {
         $this->outstream =$outstream;
-        $this->parameters += array("resource" => $src, "type" => "url");
+        $this->parameters += array("resource" => $src, "type" => "url","parameters" => $apiParameters );
         return $this->curlHttpRequest('POST', "fileResponse");
        }
-       function convertHTML($src, $outstream = null){
-        $this->outstream =$outstream;
-        $this->parameters += array("resource" => $src, "type" => "html");
-        return $this->curlHttpRequest('POST', "fileResponse");
-       }
-       function getInfo() {
-        $this->url = $this->gateway.$this->apiVersion.'/'.$this->resourceType.
-        '/'.$this->parameters["username"].'&key='.$this->parameters["key"];
-        return $this->curlHttpRequest('GET',"userInfoResponse");
-       }
-       private function getCurlGetOptions(){
-         $ret = array( CURLOPT_URL => $this->url,
-                      CURLOPT_FAILONERROR => true,
-                      CURLOPT_RETURNTRANSFER => true,
-                      CURLOPT_BINARYTRANSFER => 1,
-                      CURLOPT_TIMEOUT => 3,
-                      CURLOPT_HTTPHEADER => array('Accept: application/json'),
-                    );
-        return $ret;
-       }
-       private function getCurlPostOptions(){
 
-            $ret = array( CURLOPT_URL => $this->url,
-                      CURLOPT_FAILONERROR => true,
-                      CURLOPT_RETURNTRANSFER => true,
-                      CURLOPT_TIMEOUT => 40,
-                      CURLOPT_HTTPHEADER => array('Accept: application/json'),
-                      CURLOPT_POST => true,
-                      CURLOPT_POSTFIELDS => $this->parameters
-                    );
-            return $ret;
+
+       function convertHTML($src, $outstream = null,$apiParameters = null ){
+        $this->outstream =$outstream;
+        $this->parameters += array("resource" => $src, "type" => "html","parameters" => $apiParameters);
+        return $this->curlHttpRequest('POST', "fileResponse");
+       }
+
+       function getInfo() {
+        $this->url = $this->gateway.$this->apiVersion.'/me/info?'.
+        'username='.$this->parameters["username"].'&key='.$this->parameters["key"];
+        return $this->curlHttpRequest('GET',"userInfoResponse");
        }
        private function curlHttpRequest($method,$responseFunction ) {
 
           $this->parameters = http_build_query($this->parameters, '', '&');
+
+          $curlOpts = array(
+            CURLOPT_URL => $this->url,
+            CURLOPT_FAILONERROR => true,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HTTPHEADER => array('Accept: application/json'),
+            );
           $ch = curl_init();    // create curl resource
           switch ($method) {
-            case 'POST':   curl_setopt_array($ch, $this->getCurlPostOptions());
+            case 'POST':
+                $curlOpts[CURLOPT_TIMEOUT] = 40;
+                $curlOpts[CURLOPT_POSTFIELDS] = $this->parameters;
+                curl_setopt_array($ch, $curlOpts);
               break;
-            case 'GET':    curl_setopt_array($ch, $this->getCurlGetOptions());
+
+            case 'GET':
+                $curlOpts[CURLOPT_TIMEOUT] = 10;
+                $curlOpts[CURLOPT_BINARYTRANSFER]= 1 ;
+                curl_setopt_array($ch, $curlOpts);
               break;
             default:
               return NULL;
@@ -123,7 +118,7 @@
     $out_file = fopen("document.pdf", "wb");
     $client->convertURI("http://www.editage.com", $out_file);
     fclose($out_file);
-
+*/
     // File direct download example
 
       try
@@ -132,13 +127,13 @@
           $client = new sprintPDF("admin", "coldcold");
 
           // convert a web page and store the generated PDF into a $pdf variable
-          $pdf = $client->convertURI('http://google.com/');
+          $pdf = $client->convertURI('http://www.google.co.kr/',null," --orientation Landscape ");
 
           // set HTTP response headers
           header("Content-Type: application/pdf");
           header("Cache-Control: no-cache");
           header("Accept-Ranges: none");
-          header("Content-Disposition: attachment; filename=\"google_com.pdf\"");
+          //header("Content-Disposition: attachment; filename=\"google_com.pdf\"");
 
           // send the generated PDF
           echo $pdf;
@@ -150,8 +145,8 @@
 
   //  Get user information example
 
-      $client = new sprintPDF("admin", "coldcold");
-      $info =  $client->getInfo();
-      print_r($info);
-   */
+      // $client = new sprintPDF("admin", "coldcold");
+      // $info =  $client->getInfo();
+      // print_r($info);
+
 ?>
